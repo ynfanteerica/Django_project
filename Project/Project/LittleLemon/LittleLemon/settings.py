26f16ce45d36c223e23ac1f2427c7a28318230ff
ynfanteerica <112785857+ynfanteerica@users.noreply.github.com>
2023-08-02 09:35:39 -0400

    Update settings.py

--- Project/Project/LittleLemon/LittleLemon/settings.py
+++ Project/Project/LittleLemon/LittleLemon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-py_dit9(3hd6swd)hdu2f4=psv^6dcy(4&(@&d@1rqkn)roo^i'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
